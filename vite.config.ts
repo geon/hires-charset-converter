b6071e06d5ef74e986e779a04c05a538e1fb0ff8
Victor Widell <victor.widell@gmail.com>
2026-09-03 21:37:15 +0200

    Fixed repo name.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import typedCssModulesPlugin from 'vite-plugin-typed-css-modules'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import typedCssModulesPlugin from "vite-plugin-typed-css-modules";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/diet-color/",
-  plugins: [react(), typedCssModulesPlugin()],
+	base: "/hires-charset-converter/",
+	plugins: [react(), typedCssModulesPlugin()],
 });
